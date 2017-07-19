c23774d6315b9f0de27b8b768a0287d41f26bb28
Charlie Friend <charles.d.friend@gmail.com>
2017-07-18 21:32:45 -0700

    Randomly generate asteroids.

--- src/asteroids/Asteroid.java
+++ src/asteroids/Asteroid.java
@@ -1,5 +1,6 @@
 package asteroids;
 
+import java.awt.Point;
 import java.lang.Math;
 import processing.core.PApplet;
 
@@ -13,13 +14,22 @@ public class Asteroid {
 	public float direction;
 	public float speed; 
 	
+	private Point[] points;
+	
 	private PApplet app;
+	
+	// constants
+	private final int ASTEROID_DETAIL = 3;
+	private final int NUM_ASTEROID_POINTS = 20; // level of detail in asteroids
+	private final int MIN_ASTEROID_SIZE = 10; // minimum asteroid width (px)
+	private final int MAX_ASTEROID_SIZE = 20; // maximum asteroid width (px)
 
 	public Asteroid(PApplet pApp, float x, float y, float direction, float speed) {
 		app = pApp;
 		
-		width = 15;
-		height = 15;
+		this.width = app.random(MIN_ASTEROID_SIZE, MAX_ASTEROID_SIZE);
+		this.height = this.width;
+		this.points = generateAsteroidPoints(this.width);
 		
 		// define asteroid position
 		this.x = x;
@@ -34,7 +44,11 @@ public class Asteroid {
 	}
 	
 	public void draw() {
-		app.ellipse(x, y, 15, 15);
+		app.beginShape();
+		for (Point p : points) {
+			app.vertex(this.x + p.x, this.y + p.y);
+		}
+		app.endShape();
 	}
 	
 	public boolean isOutsideFrame() {
@@ -45,6 +59,30 @@ public class Asteroid {
 		else {
 			return false;
 		}
+	}
+	
+	// Randomly generate points to draw an asteroid
+	private Point[] generateAsteroidPoints(float width) {
+		Point[] points = new Point[NUM_ASTEROID_POINTS];
+		
+		int pointNumber = 0; // array index
+		float angleIncrement = 360 / NUM_ASTEROID_POINTS;
+		for (float angle = 0; angle < 360; angle += angleIncrement) {
+			
+			float angleRadians = PApplet.radians(angle);
+			
+			float pointX = (float) (Math.cos(angleRadians) * (width - ASTEROID_DETAIL))
+					+ app.random(-ASTEROID_DETAIL, ASTEROID_DETAIL);
+			
+			float pointY = (float) (Math.sin(angleRadians)  * (width - ASTEROID_DETAIL))
+					+ app.random(-ASTEROID_DETAIL, ASTEROID_DETAIL);
+			
+			points[pointNumber] = new Point((int)pointX, (int)pointY);
+			pointNumber += 1;
+			
+		}
+		
+		return points;
 	}
 	
 }
